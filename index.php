38e6392302b2b00b866528d10a95fb2b412a0509
NirmallyaMitra <nirmallyamitra.github@gmail.com>
2021-12-20 22:22:45 +0530

    Edit Index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 	<!-- CSS Stylesheet-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
-	<link rel="stylesheet" href="CSS/login_page.css" />
+	<link rel="stylesheet" href="CSS/Login.css" />
 
 	<!-- Google Fonts -->
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
