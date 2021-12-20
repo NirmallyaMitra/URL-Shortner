05f6bdb8e85f9a3fff127ee9a28b1041a908c904
NirmallyaMitra <nirmallyamitra.github@gmail.com>
2021-12-20 21:51:55 +0530

    Files Created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="CSS/login_page.css" />
+	<title>URL-Shortner</title>
+</head>
+<body>
+
+</body>
+</html>
